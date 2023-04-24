b6b3a187e8528afe948be34b3eed6df80da23f7f
SalihAlemdar <61salihalemdar61@gmail.com>
2023-04-24 17:47:26 +0300

    login

---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:last_project/components/my_button.dart';
+import 'package:last_project/components/my_textfield.dart';
+import 'package:last_project/components/square_title.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+//Text editin controller
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+//sign user in method
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+
+              //logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+
+              //tekrar hoşgeldin özlendin!
+              const Text(
+                'Hoşgeldiniz...',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              //User name textfield
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Kullanıcı Adı',
+                obscureText: false,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              //password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Şifre',
+                obscureText: false,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              //forgot password ?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Şifreni mi unuttun',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 15,
+              ),
+
+              //sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+
+              //or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 52.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 1.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0,
+                      ),
+                      child: Text(
+                        'Diğer Seçenekler',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 1.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+              //google + apple sign in buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  //google button
+                  SquareTitle(
+                    imagePath: 'lib/images/google.png',
+                  ),
+
+                  SizedBox(width: 30),
+
+                  //appele button
+                  SquareTitle(
+                    imagePath: 'lib/images/apple.png',
+                  ),
+                ],
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+
+              //not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Üye değil misin ?',
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Kayıt Ol',
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
